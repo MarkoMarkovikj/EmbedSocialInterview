25cd6f806ed6ff9b84ddee3d059ad6d1f8de06e0
MarkoMarkovikj <marko.markovikj.1@students.finki.ukim.mk>
2021-08-19 02:25:00 +0200

    Added json parsing and data filtering
    - Added functionality to the filter button
    - Can filter by all categories

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
+import json
 import tkinter as tk
 
-WINHEIGHT = 800
-WINWIDTH = 600
+WINHEIGHT = 900
+WINWIDTH = 700
 FONT_LG = 'Helvetica 16 bold'
 FONT_SM = 'Helvetica 10'
 
-def filter_reviews(order_by_rating, min_rating, order_by_date, prioritize_by_text):
-    print(order_by_rating, min_rating, order_by_date, prioritize_by_text, sep='\n', end='\n\n')
+
+def load_reviews():
+    with open('reviews.json') as file:
+        reviews = json.load(file)
+
+    return reviews
+
+
+def filter_by_text(reviews, prioritize_by_text):
+    if prioritize_by_text == 'Yes':
+        reviews.sort(key=lambda review: review['reviewText'] != '', reverse=True)
+
+
+def filter_by_rating(reviews, order_by_rating):
+    reverse = order_by_rating == 'Highest First'
+
+    reviews.sort(key=lambda review: review['rating'], reverse=reverse)
+
+
+def filter_by_date(reviews, order_by_date):
+    reverse = order_by_date == 'Newest First'
+
+    reviews.sort(key=lambda review: review['reviewCreatedOnTime'], reverse=reverse)
+
+
+def filter_by_min_rating(reviews, min_rating):
+    return list(filter(lambda r: r['rating'] >= min_rating, reviews))
+
+
+def filter_reviews(order_by_rating, min_rating, order_by_date, prioritize_by_text, window):
+    reviews = load_reviews()
+
+    filter_by_text(reviews, prioritize_by_text)
+    filter_by_rating(reviews, order_by_rating)
+    filter_by_date(reviews, order_by_date)
+    filtered_reviews = filter_by_min_rating(reviews, int(min_rating))
+
+    print(filtered_reviews)
 
 
 def create_window():
@@ -17,12 +54,14 @@ def create_window():
     canvas.pack()
 
     frame = tk.Frame(window)
-    frame.place(relx=0.1, rely=0, relwidth=0.8, relheight=0.9)
+    frame.place(relx=0.1, rely=0, relwidth=0.8, relheight=0.7)
 
+    # ----------------------------------------------------------------------------------
     # Title
     title_label = tk.Label(frame, text='Filter reviews', font=FONT_LG)
     title_label.place(relx=0.05, rely=0.05, relwidth=0.3, relheight=0.1)
 
+    # ----------------------------------------------------------------------------------
     # Order by rating
     order_by_rating_options = ['Highest First', 'Lowest First']
 
@@ -35,6 +74,7 @@ def create_window():
     order_by_rating_dropdown = tk.OptionMenu(frame, order_by_rating_var, *order_by_rating_options)
     order_by_rating_dropdown.place(relx=0.05, rely=0.23, relwidth=0.9, relheight=0.08)
 
+    # ----------------------------------------------------------------------------------
     # Minimum rating
     minimum_rating_options = ['1', '2', '3', '4', '5']
 
@@ -47,8 +87,9 @@ def create_window():
     minimum_rating_dropdown = tk.OptionMenu(frame, minimum_rating_var, *minimum_rating_options)
     minimum_rating_dropdown.place(relx=0.05, rely=0.43, relwidth=0.9, relheight=0.08)
 
+    # ----------------------------------------------------------------------------------
     # Order by date
-    order_by_date_options = ['Yes', 'No']
+    order_by_date_options = ['Oldest First', 'Newest First']
 
     order_by_date_label = tk.Label(frame, text='Order by date', font=FONT_SM)
     order_by_date_label.place(relx=0.05, rely=0.55, relwidth=0.2, relheight=0.08)
@@ -59,8 +100,9 @@ def create_window():
     order_by_date_dropdown = tk.OptionMenu(frame, order_by_date_var, *order_by_date_options)
     order_by_date_dropdown.place(relx=0.05, rely=0.63, relwidth=0.9, relheight=0.08)
 
+    # ----------------------------------------------------------------------------------
     # Prioritize by text
-    prioritize_by_text_options = ['Highest First', 'Lowest First']
+    prioritize_by_text_options = ['Yes', 'No']
 
     prioritize_by_text_label = tk.Label(frame, text='Prioritize by text', font=FONT_SM)
     prioritize_by_text_label.place(relx=0.05, rely=0.75, relwidth=0.2, relheight=0.08)
@@ -71,13 +113,15 @@ def create_window():
     prioritize_by_text_dropdown = tk.OptionMenu(frame, prioritize_by_text_var, *prioritize_by_text_options)
     prioritize_by_text_dropdown.place(relx=0.05, rely=0.83, relwidth=0.9, relheight=0.08)
 
+    # ----------------------------------------------------------------------------------
     # Filter button
     filter_button = tk.Button(frame, text='Filter', font=FONT_SM, bg='blue', fg='white',
                               command=lambda: filter_reviews(
                                   order_by_rating_var.get(),
                                   minimum_rating_var.get(),
                                   order_by_date_var.get(),
-                                  prioritize_by_text_var.get())
+                                  prioritize_by_text_var.get(),
+                                  window)
                               )
 
     filter_button.place(relx=0.05, rely=0.93, relwidth=0.15, relheight=0.05)
